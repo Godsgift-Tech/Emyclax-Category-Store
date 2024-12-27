b207857c2b69f231dea57797f185ca3774a4edc7
Godsgift Oghenechohwo <godsgiftoghenechohwo@gmail.com>
2024-12-27 16:17:29 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Emyclax_Category_Store.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
